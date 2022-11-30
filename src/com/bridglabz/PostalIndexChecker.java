ef5cbdb34058b21b4b3914fbb491a2974c30adc9
Kritika <kritsme963@gmail.com>
2022-11-30 09:16:58 -0800

    Added:Regex practice

---
+++ src/com/bridglabz/PostalIndexChecker.java
@@ -0,0 +1,18 @@
+package com.bridglabz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PostalIndexChecker {
+
+    public boolean isValidPin(String zip) {
+
+        Pattern zipPattern = Pattern.compile("^[0-9]{3}(\s)?[0-9]{3}$");
+
+        Matcher zipMatcher = zipPattern.matcher(zip);
+
+        return zipMatcher.matches();
+    }
+
+}
+
